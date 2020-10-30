25940da82e2ab995911325777038e015c0d5e2f9
ReyDoran <55508840+ReyDoran@users.noreply.github.com>
2020-10-30 21:15:19 +0100

    Añadidas lista de preguntas cargadas desde HTML + clase App
    La clase app sirve para pasar los recursos a clases donde no tienen acceso.

--- Frikial20/app/src/main/java/com/example/frikial20/Preguntas.java
+++ Frikial20/app/src/main/java/com/example/frikial20/Preguntas.java
@@ -11,9 +11,25 @@ public class Preguntas {
         ArrayList<Pregunta> questionList = new ArrayList<Pregunta>();
 
         Pregunta P_1 = new Pregunta(
-                1,"¿Qué personaje es el que tiene un jetpack y es capaz de dar saltos a gran altura?",0,"Boba Fett", "Darth Vader","Luke Skywalker", "Angelina Jolie", 1
+                1, App.getResourcesStatic().getString(R.string.Q_1),0,App.getResourcesStatic().getString(R.string.AW1_1), App.getResourcesStatic().getString(R.string.AW2_1),App.getResourcesStatic().getString(R.string.AW3_1), App.getResourcesStatic().getString(R.string.AW4_1), 1
         );
         questionList.add(P_1);
+        Pregunta P_2 = new Pregunta(
+                2, App.getResourcesStatic().getString(R.string.Q_2),0,App.getResourcesStatic().getString(R.string.AW1_2), App.getResourcesStatic().getString(R.string.AW2_2),App.getResourcesStatic().getString(R.string.AW3_2), App.getResourcesStatic().getString(R.string.AW4_2), 3
+        );
+        questionList.add(P_2);
+        Pregunta P_3 = new Pregunta(
+                3, App.getResourcesStatic().getString(R.string.Q_3),0,App.getResourcesStatic().getString(R.string.AW1_3), App.getResourcesStatic().getString(R.string.AW2_3),App.getResourcesStatic().getString(R.string.AW3_3), App.getResourcesStatic().getString(R.string.AW4_3), 1
+        );
+        questionList.add(P_3);
+        Pregunta P_4 = new Pregunta(
+                4, App.getResourcesStatic().getString(R.string.Q_4),0,App.getResourcesStatic().getString(R.string.AW1_4), App.getResourcesStatic().getString(R.string.AW2_4),App.getResourcesStatic().getString(R.string.AW3_4), App.getResourcesStatic().getString(R.string.AW4_4), 3
+        );
+        questionList.add(P_4);
+        Pregunta P_5 = new Pregunta(
+                5, App.getResourcesStatic().getString(R.string.Q_5),0,App.getResourcesStatic().getString(R.string.AW1_5), App.getResourcesStatic().getString(R.string.AW2_5),App.getResourcesStatic().getString(R.string.AW3_5), App.getResourcesStatic().getString(R.string.AW4_5), 4
+        );
+        questionList.add(P_5);
 
 
         return  questionList;
