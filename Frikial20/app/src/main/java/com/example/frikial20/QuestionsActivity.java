a1bc9471d46c850a7d87802becbed8ddcd48f823
Prothoky <52759261+Prothoky@users.noreply.github.com>
2020-10-30 17:06:49 +0100

    MainActivity ya esta bonito

--- Frikial20/app/src/main/java/com/example/frikial20/QuestionsActivity.java
+++ Frikial20/app/src/main/java/com/example/frikial20/QuestionsActivity.java
@@ -4,14 +4,18 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class pregunta1 extends AppCompatActivity {
+public class QuestionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pregunta1);
+        View v = findViewById(android.R.id.content);
+        TextView question = (TextView) v.findViewById(R.id.textView3);
+        question.setText(getResources().getResourceName(v.getId()));
     }
 
     public void checkAnswer(View view) {
