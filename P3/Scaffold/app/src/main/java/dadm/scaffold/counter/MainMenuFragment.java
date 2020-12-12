36c6f76bfbc22913fc33195c9844575d6fc681bb
Prothoky <Prothoky@github.com>
2020-12-13 00:10:41 +0100

    button exit

--- P3/Scaffold/app/src/main/java/dadm/scaffold/counter/MainMenuFragment.java
+++ P3/Scaffold/app/src/main/java/dadm/scaffold/counter/MainMenuFragment.java
@@ -30,10 +30,17 @@ public class MainMenuFragment extends BaseFragment implements View.OnClickListen
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         view.findViewById(R.id.btn_start).setOnClickListener(this);
+        view.findViewById(R.id.btn_exit).setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        ((ScaffoldActivity)getActivity()).choseShip();
+        if(v.getId() == R.id.btn_start )
+            ((ScaffoldActivity)getActivity()).choseShip();
+        if(v.getId() == R.id.btn_exit ){
+            ((ScaffoldActivity)getActivity()).finish();
+        }
+
     }
 }
